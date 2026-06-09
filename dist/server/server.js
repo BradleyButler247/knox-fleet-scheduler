f72bdce0bb545a96c68ff7e95a857bb4dae1a89a
Bradley Butler <bradley@Bradleys-MacBook-Pro.local>
2026-06-09 10:36:10 -0600

    update netlify.toml and vite config

--- dist/server/server.js
+++ dist/server/server.js
@@ -53,7 +53,7 @@ function renderErrorPage() {
 let serverEntryPromise;
 async function getServerEntry() {
   if (!serverEntryPromise) {
-    serverEntryPromise = import("./assets/server-BUgqK2Jh.js").then((n) => n.s).then(
+    serverEntryPromise = import("./assets/server-oo53HHGj.js").then((n) => n.s).then(
       (m) => m.default ?? m
     );
   }
