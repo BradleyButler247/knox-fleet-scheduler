2bcd0474aba758b8d9794e12c19fcb27242e0780
Bradley Butler <bradley@Bradleys-MacBook-Pro.local>
2026-06-09 11:07:41 -0600

    update config

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-import tsconfigPaths from 'vite-tsconfig-paths'
 import netlify from '@netlify/vite-plugin-tanstack-start'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
   plugins: [
-    tsconfigPaths(),
+    tsconfigPaths({
+      projects: ['./tsconfig.json'],
+    }),
     tanstackStart(),
     netlify(),
     react(),
