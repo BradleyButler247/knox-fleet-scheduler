c351bda8de5a79c61a4ad476304c7ce4b11959da
Bradley Butler <bradley@Bradleys-MacBook-Pro.local>
2026-07-12 20:59:27 -0600

    update router.tsx

--- src/router.tsx
+++ src/router.tsx
@@ -1,8 +1,8 @@
-import { createRouter } from '@tanstack/react-router'
+import { createRouter as createTanStackRouter } from '@tanstack/react-router'
 import { routeTree } from './routeTree.gen'
 
-export function getRouter() {
-  const router = createRouter({
+export function createRouter() {
+  const router = createTanStackRouter({
     routeTree,
     scrollRestoration: true,
   })
@@ -12,6 +12,6 @@ export function getRouter() {
 
 declare module '@tanstack/react-router' {
   interface Register {
-    router: ReturnType<typeof getRouter>
+    router: ReturnType<typeof createRouter>
   }
 }
